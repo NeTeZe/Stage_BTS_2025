517ea2cca08975a71cfa59b629b5402094682a94
Nicolas Bonvalet <nicobonvalperso@gmail.com>
2025-05-19 15:42:56 +0200

    Ajout des commentaires pour + de compréhension

--- src/src.py
+++ src/src.py
@@ -1,7 +1,11 @@
-# === INCLUDES === # 
+# === INCLUDES === #
+#  
 import pyshark 
 
 # === FONCTIONS === #
+
+# Fonction menu qui permet d'afficher un menu
+# et de return le choix effectué  
 def menu():
     pick = -1
     listPick = [1]
@@ -11,6 +15,7 @@ def menu():
         pick = int(input("Entrer votre choix : "))
     return pick
 
+# Fonction qui permet d'afficher les trames extraites 
 def packetPrint(capture) : 
     print(" -- Affichage des trames -- ")
     for packet in capture : 
@@ -18,7 +23,10 @@ def packetPrint(capture) :
 
 
 # === MAIN === #
-capture = pyshark.FileCapture("Stage_BTS_2025\sauvegardes\Espion_03832_20250518054123.pcap")
-pick = menu()
+
+# extraction de la capture .pcap et stocker dans la variable capture
+capture = pyshark.FileCapture("Stage_BTS_2025\sauvegardes\Espion_03832_20250518054123.pcap") 
+# appel à la fonction menu
+pick = menu() 
 if(pick == 1) : 
     packetPrint(capture)
