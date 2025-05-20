0969626673076b1986acd27ab704c9e0bb108eab
Nicolas Bonvalet <nicobonvalperso@gmail.com>
2025-05-20 09:32:23 +0200

    Ajout de la fonction pick, modif de la fonction menu et upgrade de la fonction packetPrint qui permet de d'afficher les champs exactes du paquet

--- src/src.py
+++ src/src.py
@@ -1,32 +1,45 @@
 # === INCLUDES === #
-#  
+#  DOCUMENTATION  https://github.com/KimiNewt/pyshark?tab=readme-ov-file
 import pyshark 
 
 # === FONCTIONS === #
-
-# Fonction menu qui permet d'afficher un menu
-# et de return le choix effectué  
-def menu():
+# Fonction pick qui permet de retourner
+# une valeur saisie par le user
+def pick() : 
+    listPick = [1,2]
     pick = -1
-    listPick = [1]
-    print(" - MENU - ")
-    print(" 1 - Lecture des trames ")
     while listPick.count(pick) != 1 : 
         pick = int(input("Entrer votre choix : "))
     return pick
 
+# Fonction menu qui permet d'afficher un menu
+def menu():
+    print(" - MENU - ")
+    print(" 1 - Lecture des trames ")
+
 # Fonction qui permet d'afficher les trames extraites 
 def packetPrint(capture) : 
     print(" -- Affichage des trames -- ")
-    for packet in capture : 
-        print(packet)
+    print(" 1 - Affichage de toutes les trames ")
+    print(" 2 - Affichage d'une trame spécifiques ")
+    pickPacketPrint = pick()
+    if(pickPacketPrint == 1) : 
+        for packet in capture : print(packet) #affichage des toutes les trames
+    elif(pickPacketPrint == 2):
+        n = int(input("Entrer le numéro de la trame voulue :  "))
+        print(capture[5][5].Filename)
+    else: 
+        exit()
+
 
 
 # === MAIN === #
 
 # extraction de la capture .pcap et stocker dans la variable capture
-capture = pyshark.FileCapture("Stage_BTS_2025\sauvegardes\Espion_03832_20250518054123.pcap") 
+capture = pyshark.FileCapture("Stage_BTS_2025\sauvegardes\Espion_03832_20250518054123.pcap",display_filter='smb2') 
 # appel à la fonction menu
-pick = menu() 
-if(pick == 1) : 
+menu() 
+pickMain = pick()
+print("pick",pickMain)
+if(pickMain == 1) : 
     packetPrint(capture)
