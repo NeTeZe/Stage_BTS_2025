f0769bda85d098f93b32a14b4c56c339014ede01
Nicolas Bonvalet <nicobonvalperso@gmail.com>
2025-05-20 13:00:24 +0200

    Arrangement du programme ( nettoyage des fonctions inutiles) et amélioration de notre mini-bdd

--- src/src.py
+++ src/src.py
@@ -2,6 +2,7 @@
 #  DOCUMENTATION  https://github.com/KimiNewt/pyshark?tab=readme-ov-file
 import pyshark 
 
+
 # === FONCTIONS === #
 # Fonction pick qui permet de retourner
 # une valeur saisie par le user
@@ -16,21 +17,18 @@ def pick(listPick) :
 def menuMain():
     print(" - MENU - ")
     print(" 1 - Lecture des trames ")
-    print(" 90 - TEST : Afficher les attributs")
+    print(" 2 - Construction d'une mini-bdd des packets ")
+    print(" 90 - TEST : ")
 
-# Fonction menu qui permet d'afficher le menu de la fonction attributePrint
-def menuAttribute(): 
-    print(" -- Affichage des attributes par protocole -- ")
-    print(" 1 - Ethernet ")
-    print(" 2 - IP ")
-    print(" 3 - TCP ")
-    print(" 4 - SMB2 ")
-    
 # Fonction menu qui permet d'afficher le menu de la fonction packetPrint
 def menuPacketPrint(): 
     print(" -- Affichage des trames -- ")
     print(" 1 - Affichage de toutes les trames ")
     print(" 2 - Affichage d'une trame spécifiques ")
+
+# Fonction menu qui permet d'afficher le menu de la fonction menuPacketInfoBuilder
+def menuPacketInfoBuilder() : 
+    print(" -- Construction d'une mini bdd de la capture -- ")
 
 # Fonction qui permet d'afficher les trames extraites 
 def packetPrint(capture) : 
@@ -39,7 +37,6 @@ def packetPrint(capture) :
     if(pickPacketPrint == 1) : 
         for packet in capture : print(packet) #affichage des toutes les trames
     elif(pickPacketPrint == 2):
-        print(dir(capture[5].eth))
         print("Adr IP SRC : ",capture[5].ip.src)
         print("Adr IP SRC : ",capture[5].ip.src)
         print("Adr IP DST : ",capture[5].ip.dst)
@@ -52,32 +49,41 @@ def packetPrint(capture) :
     print("\n")
 
 
-def attributePrint(capture) : 
-    menuAttribute()
-    proto = pick([1,2,3,4])
-    if(proto == 1) : 
-        print(dir(capture[5].eth))
-    elif(proto == 2) : 
-        print(dir(capture[5].ip))
-    elif(proto == 3) : 
-        print(dir(capture[5].tcp))
-    elif(proto == 4) : 
-        print(dir(capture[5].smb2))
-    else :
-        exit()
+def packetInfoBuilder(capture) : 
+    menuPacketInfoBuilder()
+    bdd = []
+    i=1
+    for packet in capture : 
+        bdd.append({"IDENT : ": i})
+        if ('IP' in packet) : 
+            bdd.append({"IP SRC : ": packet.ip.src, "IP DST : ":packet.ip.dst})
+        if('ETH' in packet) : 
+            bdd.append({"MAC SRC : ":packet.eth.src,"MAC DST : ":packet.eth.dst})
+        if('TCP' in packet) : 
+            bdd.append({"PORT SRC : ":packet.tcp.srcport,"PORT DST : ":packet.tcp.dstport})
+        i += 1
+    
+    for trames in bdd:
+        for info in trames.items() : 
+            print(info)
+        
+
+
 # === MAIN === #
 print("--- DEBUT DU PROGRAMME ---")
 # extraction de la capture .pcap et stocker dans la variable capture
-capture = pyshark.FileCapture("Stage_BTS_2025\sauvegardes\Espion_03832_20250518054123.pcap",display_filter='smb2') 
+capture = pyshark.FileCapture("Stage_BTS_2025\sauvegardes\Espion_03832_20250518054123.pcap") 
 # appel à la fonction menu
 continu = "y"
 while continu != "n" : 
     menuMain() 
-    pickMain = pick([1,90])
+    pickMain = pick([1,2,90])
     if(pickMain == 1) : 
         packetPrint(capture)
+    elif(pickMain == 2) : 
+        packetInfoBuilder(capture)
     elif(pickMain == 90) :
-        attributePrint(capture)
+        print("TEST")
     continu = input("Voulez-vous continuer le programme ? : (y/n) ")
 
 
